de2de378fd090341d2eb52bbb686fffbfce68986
Calvin Kuhnen <calvinkuhnen@gmail.com>
2023-02-01 15:56:48 -0500

    serving react build

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,12 @@
+const path = require('path');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -11,6 +15,10 @@ app.use(function(req, res, next) {
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
